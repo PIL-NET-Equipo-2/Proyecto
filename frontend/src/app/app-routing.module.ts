317f7d02f6791e1b9bcdaaa73223a156e25e75fc
FeliPrimo25 <feliprimo52@gmail.com>
2023-08-27 10:40:23 -0300

    Agregado de ruta cotizaciones

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CotizacionesComponent } from './pages/cotizaciones/cotizaciones.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cotizacionse', component: CotizacionesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
