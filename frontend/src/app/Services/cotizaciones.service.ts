9d1611bd6a6ce81a000170ea006bbca1e5be7775
FeliPrimo25 <feliprimo52@gmail.com>
2023-08-27 11:10:36 -0300

    Creacion del metodo  para obtener las cotizaciones en el servicio cotizaciones

--- frontend/src/app/Services/cotizaciones.service.ts
+++ frontend/src/app/Services/cotizaciones.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'
+import {Observable} from 'rxjs'
 @Injectable({
   providedIn: 'root'
 })
 export class CotizacionesService {
+  urlApi:string = "http://localhost:3000/"
+  constructor(
+    private http:HttpClient
+  ) { }
 
-  constructor() { }
+  obtenerCotizaciones():Observable<any>{
+    return this.http.get(`${this.urlApi}titulos`)
+  }
+
 }
